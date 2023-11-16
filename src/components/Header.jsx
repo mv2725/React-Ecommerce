a7270a314a08160358475ed090c059ac953a647a
meetvyas416@gmail.com <meetvyas416@gmail.com>
2023-11-16 21:29:29 +0530

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ const handleLogout = () => {
             <a href="#">laptops</a>
             <a href="#">fragrances</a>
             <a href="#">skincare</a>
-            <a href="#">groceries</a>
+            <a href="#">gro</a>
             {/* <a href="#">home-decoration</a> */}
           </div>
         </div>
--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function ProductDetail() {
   return (
-    <div>productDetail</div>
+    <div>productDEtailoptios</div>
   )
 }
