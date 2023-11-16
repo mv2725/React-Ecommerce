4c8d3de5e28d8b87b27c1e8919e5951c3f872c05
meetvyas416@gmail.com <meetvyas416@gmail.com>
2023-11-16 15:41:16 +0530

    done

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,12 +4,9 @@
 sudo chmod -R 777 /home/ubuntu/React-Ecommerce
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
+cd /home/ubuntu/React-Ecommerce
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
 
 
 npm install
