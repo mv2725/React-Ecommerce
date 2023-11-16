7be5adc0fe3fa6f1e3489be2a81994745234a63e
meetvyas416@gmail.com <meetvyas416@gmail.com>
2023-11-16 20:22:14 +0530

    done

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,14 +2,13 @@
 
 # Navigate to the application directory
 cd /home/ubuntu/React-Ecommerce
-chmod -R 777 node_modules
+chmod -R 777 .
 
 # Add npm and node to path using NVM
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Install node modules
-npm install
-
+sudo npm install
 # Start the Node.js application
 npm start
