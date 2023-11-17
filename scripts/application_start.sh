3adfd0478da4a2e9235faa34aa9eb2ef0d1c1460
Meet vyas~ <meetvyas255@gmail.com~>
2023-11-17 00:24:42 -0600

    changes in login title

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,9 +1,10 @@
-User
 #!/bin/bash
+
+# Exit on error
+set -e
 
 # Navigate to the application directory
 cd /home/ubuntu/React-Ecommerce
-
 
 # Add npm and node to path using NVM
 export NVM_DIR="$HOME/.nvm"
@@ -11,8 +12,19 @@ export NVM_DIR="$HOME/.nvm"
 
 # Install node modules
 npm install
+
 # Start the Node.js application
-npm start -- your-app-name > /dev/null 2>&1 &
-sleep 10
-echo "Node.js application started successfully."
-exit 0
+npm start -- your-app-name > /home/ubuntu/React-Ecommerce/app.log 2>&1 &
+pid=$!
+
+# Wait for the application to start (you can adjust the sleep time based on your application's startup time)
+sleep 15
+
+# Check if the application is running
+if ps -p $pid > /dev/null; then
+    echo "Node.js application started successfully with PID $pid."
+    exit 0
+else
+    echo "Failed to start Node.js application. Check app.log for details."
+    exit 1
+fi
